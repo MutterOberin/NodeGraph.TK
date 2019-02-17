6ad13188459ca1d13e3d44fe0a86c2214727f348
Wito Engelke <wito.engelke@googlemail.com>
2019-02-17 15:50:45 +0100

    Buggy

--- NodeGraph.TK.Example/Form1.Designer.cs
+++ NodeGraph.TK.Example/Form1.Designer.cs
@@ -28,9 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            NodeGraph.TK.NodeGraphGraph nodeGraphGraph3 = new NodeGraph.TK.NodeGraphGraph();
+            NodeGraph.TK.NodeGraphGraph nodeGraphGraph1 = new NodeGraph.TK.NodeGraphGraph();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            NodeGraph.TK.NodeGraphGraph nodeGraphGraph2 = new NodeGraph.TK.NodeGraphGraph();
             this.nodeGraphPanel1 = new NodeGraph.TK.NodeGraphPanel();
+            this.nodeGraphPanel2 = new NodeGraph.TK.NodeGraphPanel();
             this.SuspendLayout();
             // 
             // nodeGraphPanel1
@@ -45,7 +47,7 @@
             this.nodeGraphPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.nodeGraphPanel1.DrawShadow = true;
             this.nodeGraphPanel1.EnableDrawDebug = true;
-            this.nodeGraphPanel1.Graph = nodeGraphGraph3;
+            this.nodeGraphPanel1.Graph = nodeGraphGraph1;
             this.nodeGraphPanel1.GridAlpha = 0.125F;
             this.nodeGraphPanel1.GridPadding = 64F;
             this.nodeGraphPanel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
@@ -77,6 +79,50 @@
             this.nodeGraphPanel1.SmoothBehavior = false;
             this.nodeGraphPanel1.TabIndex = 0;
             // 
+            // nodeGraphPanel2
+            // 
+            this.nodeGraphPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.nodeGraphPanel2.ConnectorFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.nodeGraphPanel2.ConnectorFillSelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.nodeGraphPanel2.ConnectorHitZoneBleed = 2;
+            this.nodeGraphPanel2.ConnectorOutlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.nodeGraphPanel2.ConnectorOutlineSelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.nodeGraphPanel2.ConnectorTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.nodeGraphPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nodeGraphPanel2.DrawShadow = true;
+            this.nodeGraphPanel2.EnableDrawDebug = true;
+            this.nodeGraphPanel2.Graph = nodeGraphGraph2;
+            this.nodeGraphPanel2.GridAlpha = 0.125F;
+            this.nodeGraphPanel2.GridPadding = 64F;
+            this.nodeGraphPanel2.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.nodeGraphPanel2.LinkEditableColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(255)))), ((int)(((byte)(0)))));
+            this.nodeGraphPanel2.LinkHardness = 2F;
+            this.nodeGraphPanel2.LinkVisualStyle = NodeGraph.TK.LinkVisualStyle.Curve;
+            this.nodeGraphPanel2.Location = new System.Drawing.Point(0, 0);
+            this.nodeGraphPanel2.Name = "nodeGraphPanel2";
+            this.nodeGraphPanel2.NodeConnectorFont = new System.Drawing.Font("Tahoma", 7F);
+            this.nodeGraphPanel2.NodeConnectorTextZoomTreshold = 0.8F;
+            this.nodeGraphPanel2.NodeFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.nodeGraphPanel2.NodeFillSelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(128)))), ((int)(((byte)(100)))));
+            this.nodeGraphPanel2.NodeHeaderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.nodeGraphPanel2.NodeHeaderSize = 24;
+            this.nodeGraphPanel2.NodeOutlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.nodeGraphPanel2.NodeOutlineSelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(160)))), ((int)(((byte)(128)))));
+            this.nodeGraphPanel2.NodeScaledConnectorFont = new System.Drawing.Font("Tahoma", 7F);
+            this.nodeGraphPanel2.NodeScaledTitleFont = new System.Drawing.Font("Tahoma", 8F);
+            this.nodeGraphPanel2.NodeSignalInvalidColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.nodeGraphPanel2.NodeSignalValidColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(0)))));
+            this.nodeGraphPanel2.NodeTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.nodeGraphPanel2.NodeTextShadowColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.nodeGraphPanel2.NodeTitleFont = new System.Drawing.Font("Tahoma", 8F);
+            this.nodeGraphPanel2.NodeTitleZoomThreshold = 0.5F;
+            this.nodeGraphPanel2.SelectionFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(128)))), ((int)(((byte)(90)))), ((int)(((byte)(30)))));
+            this.nodeGraphPanel2.SelectionOutlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(180)))), ((int)(((byte)(60)))));
+            this.nodeGraphPanel2.ShowGrid = true;
+            this.nodeGraphPanel2.Size = new System.Drawing.Size(512, 512);
+            this.nodeGraphPanel2.SmoothBehavior = false;
+            this.nodeGraphPanel2.TabIndex = 0;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -91,6 +137,7 @@
         #endregion
 
         private NodeGraphPanel nodeGraphPanel1;
+        private NodeGraphPanel nodeGraphPanel2;
     }
 }
 
